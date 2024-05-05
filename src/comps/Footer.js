912b05c0119977ce79c207179169ddcfaa8bbf04
MirShukhman <miriysha@gmail.com>
2024-05-05 11:34:14 +0300

    deploy 05.05

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p>Sapir Academic College, Department of Computer Science</p>
+            <p>&copy; Powered By: Mir Shukhman 2024</p>
             <p>Project by Artyom Generalov and Itamar Abir</p>
         </footer>
     )
